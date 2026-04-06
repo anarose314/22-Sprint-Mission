65c8a0abe9575acec930708876c230b06aae9229
anarose314 <dpgp706@gmail.com>
2026-04-06 21:48:40 +0900

    🎨 Style: MainLayout 반응형 레이아웃 및 배경색 적용

--- src/shared/components/layout/main-layout/index.tsx
+++ src/shared/components/layout/main-layout/index.tsx
@@ -3,10 +3,14 @@ import Header from '@/shared/components/layout/header';
 
 export default function MainLayout() {
   return (
-    <>
+    <div className="flex min-h-screen flex-col bg-[#FCFCFC]">
       <Header variant="main" />
-      <Outlet />
+      <main className="w-full flex-1 px-6 pt-6 pb-12">
+        <div className="mx-auto w-full max-w-300">
+          <Outlet />
+        </div>
+      </main>
       <ScrollRestoration />
-    </>
+    </div>
   );
 }
