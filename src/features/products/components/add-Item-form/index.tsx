1445dc9135dcf9c64fcb7f19772ac4ab0af7e86d
anarose314 <dpgp706@gmail.com>
2026-04-13 15:52:47 +0900

    ✨ Feat: addItem 페이지 공통 컴포넌트 활용하여 작성

---
+++ src/features/products/components/add-Item-form/index.tsx
@@ -0,0 +1,47 @@
+import { Button } from '@/shared/components/button';
+import { ImageUploader } from '@/shared/components/image-uploader';
+import { Input } from '@/shared/components/input';
+
+export function AddItemForm() {
+  return (
+    <>
+      <div className="mb-6 flex items-center justify-between">
+        <h2 className="typo-xl-bold text-secondary-900">상품 등록하기</h2>
+        <Button type="submit" size="small40" disabled>
+          등록
+        </Button>
+      </div>
+      <form className="flex flex-col gap-8">
+        <ImageUploader label="상품 이미지" />
+        <Input
+          label="상품명"
+          placeholder="상품명을 입력해주세요"
+          errorMessage=""
+          required
+        />
+        <Input
+          textarea
+          rows={10}
+          label="상품소개"
+          placeholder="상품소개를 입력해주세요"
+          errorMessage=""
+          required
+        />
+        <Input
+          label="상품명"
+          placeholder="상품명을 입력해주세요"
+          errorMessage=""
+          required
+        />
+        <Input
+          label="판매가격"
+          type="number"
+          min="0"
+          placeholder="판매 가격을 입력해주세요"
+          errorMessage=""
+          required
+        />
+      </form>
+    </>
+  );
+}
