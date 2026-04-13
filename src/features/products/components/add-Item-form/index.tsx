6f845a05331368bcec6e7044ddd0158a6cfadb96
anarose314 <dpgp706@gmail.com>
2026-04-13 16:25:20 +0900

    ✨ Feat: 폼 유효성 검사 후, 문제 없으면 등록 버튼 활성화 되게 수정

--- src/features/products/components/add-Item-form/index.tsx
+++ src/features/products/components/add-Item-form/index.tsx
@@ -1,14 +1,17 @@
 import { TagInputField } from '@/features/products/components/add-Item-form/tag-input-field';
+import { useAddItemFrom } from '@/features/products/hooks/useAddItemForm';
 import { Button } from '@/shared/components/button';
 import { ImageUploader } from '@/shared/components/image-uploader';
 import { Input } from '@/shared/components/input';
 
 export function AddItemForm() {
+  const { values, errors, handleChange, isFormVaild } = useAddItemFrom();
+
   return (
     <>
       <div className="mb-6 flex items-center justify-between">
         <h2 className="typo-xl-bold text-secondary-900">상품 등록하기</h2>
-        <Button type="submit" size="small40" disabled>
+        <Button type="submit" size="small40" disabled={!isFormVaild}>
           등록
         </Button>
       </div>
@@ -17,7 +20,9 @@ export function AddItemForm() {
         <Input
           label="상품명"
           placeholder="상품명을 입력해주세요"
-          errorMessage=""
+          value={values.name}
+          onChange={(e) => handleChange('name', e.target.value)}
+          errorMessage={errors.name || undefined}
           required
         />
         <Input
@@ -25,7 +30,9 @@ export function AddItemForm() {
           rows={10}
           label="상품소개"
           placeholder="상품소개를 입력해주세요"
-          errorMessage=""
+          value={values.description}
+          onChange={(e) => handleChange('description', e.target.value)}
+          errorMessage={errors.description || undefined}
           required
         />
         <Input
@@ -33,7 +40,9 @@ export function AddItemForm() {
           type="number"
           min="0"
           placeholder="판매 가격을 입력해주세요"
-          errorMessage=""
+          value={values.price}
+          onChange={(e) => handleChange('price', e.target.value)}
+          errorMessage={errors.price || undefined}
           required
         />
         <TagInputField />
