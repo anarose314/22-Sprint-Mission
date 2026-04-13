1cfb77ba6b0ea94c977f9fa90222e808e95f634f
anarose314 <dpgp706@gmail.com>
2026-04-13 16:47:34 +0900

    ♻️ Refactor: 빈 태그 및 최대 개수 추가 시 에러 발생 로직 추가 및 CSS 수정

--- src/features/products/components/add-Item-form/tag-input-field/index.tsx
+++ src/features/products/components/add-Item-form/tag-input-field/index.tsx
@@ -4,19 +4,31 @@ import { useState, type KeyboardEvent } from 'react';
 
 export function TagInputField() {
   const [tags, setTags] = useState<string[]>([]);
-  const [isTagError, setIsTagError] = useState(false);
+  const [tagError, setTagError] = useState('');
 
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.nativeEvent.isComposing) return;
 
     if (e.key === 'Enter') {
-      setIsTagError(false);
+      setTagError('');
       e.preventDefault();
 
       const newTag = e.currentTarget.value.trim();
 
-      if (!newTag || tags.includes(newTag)) {
-        setIsTagError(true);
+      if (!newTag) {
+        setTagError('태그를 입력해주세요');
+        e.currentTarget.value = '';
+        return;
+      }
+
+      if (tags.includes(newTag)) {
+        setTagError('이미 등록된 태그입니다');
+        e.currentTarget.value = '';
+        return;
+      }
+
+      if (tags.length >= 20) {
+        setTagError('태그를 더 이상 추가할 수 없습니다');
         e.currentTarget.value = '';
         return;
       }
@@ -31,7 +43,7 @@ export function TagInputField() {
       <Input
         label="태그"
         placeholder="태그를 입력해주세요"
-        errorMessage={isTagError ? '중복된 태그가 있습니다.' : undefined}
+        errorMessage={tagError || undefined}
         onKeyDown={handleKeyDown}
       />
       <TagList tags={tags} />
--- src/features/products/components/add-Item-form/tag-list/index.tsx
+++ src/features/products/components/add-Item-form/tag-list/index.tsx
@@ -3,7 +3,7 @@ import { IcClose } from '@/shared/assets/icons';
 
 export function TagList({ tags }: TagListProps) {
   return (
-    <ul className="mt-3.5 flex gap-3">
+    <ul className="mt-3.5 flex flex-wrap gap-3">
       {tags.map((tag) => (
         <li
           className="bg-secondary-100 typo-lg-regular text-secondary-800 inline-flex h-9 items-center justify-center gap-2 rounded-full px-4 select-none"
